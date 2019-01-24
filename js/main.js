23d3b225618609c1ca50103281c2ec30fbb62397
JSGcode <JMontesdeoca@spartaglobal.com>
2019-01-24 17:36:54 +0000

    second commit, fully OOP and added multiple functions

--- js/main.js
+++ js/main.js
@@ -7,61 +7,127 @@ document.addEventListener("DOMContentLoaded", function(){
             this.table = document.getElementsByClassName('table');
             this.card = document.getElementsByClassName('card');
             this.front = document.getElementsByClassName('front');
-            this.back = document.getElementsByClassName('back')
+            this.back = document.getElementsByClassName('back');
+            this.turnCounter = document.getElementsByClassName('turns');
+            this.allCards = document.querySelectorAll('.card');
+            this.button = document.getElementById('reset_button');
+            this.modal = document.getElementById('myModal');
+            this.btn = document.getElementById("myBtn");
+            this.span = document.getElementsByClassName("close")[0];
+            this.counter = 0;
+            this.flipped = true;
+            this.pairsMatched = 0;
+            this.firstCard;
+            this.secondCard;
         }
-        // adds click listeners to all cards and intstantiates the shuffle method//
-        getCards(){
+
+        // this will be the main method that will control the behavior of cards//
+        main(){
+            // declared variables for this method //
+            var location = this;
             this.shuffle();
-            allCards.forEach(cards => cards.addEventListener('click', this.main));
+            // for each loop adds click listeners to all cards //
+            this.allCards.forEach(cards => cards.addEventListener('click', function(){
+                // ==changes class to create the flip effect== //   
+                this.classList.add('flip');
+
+                if (location.firstCard === this) {
+                    return;
+                }
+                else if (location.flipped === true) {
+                    location.firstCard = this;
+                    location.firstCard.removeEventListener('click', this);      
+                    location.flipped = false; 
+                    location.turns();
+                }else{
+                    location.secondCard = this;
+                    location.secondCard.removeEventListener('click', this);
+                    location.flipped = true;  
+                    location.turns();
+                    //==Matching and Unmatched Logic==//
+                    if (location.flipped === true && location.firstCard.dataset.img === location.secondCard.dataset.img) {
+                        console.log('matched pair');
+                        location.reset()
+                        location.winTrigger();
+                    }else{
+                        setTimeout(function() {
+                            location.firstCard.classList.remove('flip');
+                            location.secondCard.classList.remove('flip');
+                            location.reset();
+                        }, 500);    
+                    }
+                }  
+            }));
         }
         // since cards utilizes flexbox shuffle will change the style of each card and assign a random number to the order //
         shuffle() {
-            allCards.forEach(card => {
-                console.log(this.card.length);
+            this.allCards.forEach(card => {
                 let randomPos = Math.floor(Math.random() * this.card.length);
                 card.style.order = randomPos;
             });
-          }
-        // this will be the main method that will control the behavior of cards//
-        main(){
-            function reset() {
-                flipped = true;
-                firstCard = null;
-                secondCard = null;
-            }
-            this.classList.add('flip');
-            if (firstCard === this) {
-                return;
-            }
-            else if (flipped === true) {
-                firstCard = this;
-                firstCard.removeEventListener('click', this);      
-                flipped = false; 
-                return;
-            }else{
-                secondCard = this;
-                secondCard.removeEventListener('click', this);
-                flipped = true;   
-                //==Matching and Unmatched Logic==//
-                if (flipped === true && firstCard.dataset.img === secondCard.dataset.img) {
-                    console.log('matched pair');
-                    reset()
-                }else{
-                    setTimeout(function() {
-                        firstCard.classList.remove('flip');
-                        secondCard.classList.remove('flip');
-                        reset();
-                    }, 500);    
-                }
-            }  
         }
-    }
-    let flipped = true;
-    let firstCard, secondCard;
-    const allCards = document.querySelectorAll('.card');
+        reset() {
+            this.flipped = true;
+            this.firstCard = null;
+            this.secondCard = null;
+        }
+        turns(){
+            this.counter++
+            this.turnCounter[0].innerText = this.counter;
+            console.log(this.counter);   
+        }
+        winTrigger(){
+            const location = this;
+            setTimeout(function(){
+                if (location.pairsMatched === (location.card.length/2)) {
+                    location.modalBox();
+                }
+            }, 400);
+            this.pairsMatched++;
+            console.log(this.pairsMatched);
+        }
+        hardReset(){
+            const location = this;
+            this.button.addEventListener('click', function() {
+                location.flipped = true;
+                location.firstCard = null;
+                location.secondCard = null;
+                location.counter = 0;
+                location.pairsMatched = 0;
+                location.allCards.forEach(card => {
+                    card.classList.remove('flip');
+                });
+                location.turnCounter[0].innerText = location.counter;
+                setTimeout(function() {
+                    location.shuffle();
+                }, 500);
+            });
+        }
+        modalBox(){
+            const location = this;
+            location.modal.style.display = "block";
+            location.span.onclick = function() {
+            location.modal.style.display = "none";
+            }
+            window.onclick = function(event) {
+                if (event.target == modal) {
+                    location.modal.style.display = "none";
+                }
+            }
+        }
+        
+    } //End Of Class
+
     // ===Instantiate the class=== //
     const MemoryGame = new NewGame();
-    MemoryGame.getCards();
+    MemoryGame.main();
+    console.log(MemoryGame.hardReset());
+
+    //=========== testing modal javascript ==========//
+
+
+    // When the user clicks on the button, open the modal 
+
     
 
 }) // End of DOMContentloaded
